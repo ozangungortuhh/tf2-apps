eef8cf3208821d2524c6b8143e6cc25a565d4c54
ozangungortuhh <ozan.gungor@tuhh.de>
2020-06-06 17:29:43 +0200

    Clean up the script

--- apps/display_detections_cam.py
+++ apps/display_detections_cam.py
@@ -28,7 +28,6 @@ class DetectStream():
 
         self.display_detections()
         
-    
     def display_detections(self):
         start = time.time()
         tick = 0
@@ -41,13 +40,12 @@ class DetectStream():
                 time.sleep(0.1)
                 continue
 
-            # img_tf = cv2.cvtColor(cv_img, cv2.COLOR_BGR2RGB)
-            # img_tf = tf.expand_dims(img_tf, 0)
-            # img_tf = transform_images(img_tf, 416)
+            img_tf = cv2.cvtColor(cv_img, cv2.COLOR_BGR2RGB)
+            img_tf = tf.expand_dims(img_tf, 0)
+            img_tf = transform_images(img_tf, 416)
 
-            # boxes, scores, classes, nums = self.yolo.predict(img_tf)
-            # cv_img = draw_outputs(cv_img, (boxes, scores, classes, nums), self.class_names)
-            
+            boxes, scores, classes, nums = self.yolo.predict(img_tf)
+            cv_img = draw_outputs(cv_img, (boxes, scores, classes, nums), self.class_names)
             
             # Display the detection
             cv2.imshow('output', cv_img)
@@ -71,50 +69,3 @@ if __name__ == "__main__":
         app.run(main)
     except SystemExit:
         pass
-
-
-
-
-
-
-
-
-# def main(_argv):
- 
-#     physical_devices = tf.config.experimental.list_physical_devices('GPU')
-#     for physical_device in physical_devices:
-#         tf.config.experimental.set_memory_growth(physical_device, True)
-    
-#     yolo = YoloV3(classes=80)
-#     # self.yolo = YoloV3()
-#     yolo.load_weights(WEIGHTS)
-#     class_names = [c.strip() for c in open(CLASSES).readlines()]
-#     cap = cv2.VideoCapture(0)
-
-#     while True:
-#         _, img = cap.read()
-
-#         if img is None:
-#             logging.warning("Empty Frame")
-#             time.sleep(0.1)
-#             continue
-        
-#         img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
-#         img = tf.expand_dims(img, 0)
-#         img = transform_images(img, 416)
-
-#         boxes, scores, classes, nums = yolo.predict(img)
-#         print(nums) 
-#         img = draw_outputs(img, (boxes, scores, classes, nums), class_names)
-        
-#         # Display the detection
-#         cv2.imshow('output', img)
-#         if cv2.waitKey(1) == ord('q'):
-#             break
-    
-# if __name__ == '__main__':
-#     try:
-#         app.run(main)
-#     except SystemExit:
-#         pass
-
